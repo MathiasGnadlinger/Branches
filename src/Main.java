d6cf925d62df5ff2441c12c33ee2cc11cc09d66d
Mathias Gnadlinger <mathias.gnadlinger@htl-wels.at>
2020-10-15 12:17:54 +0200

    Lauffähiges Javafx

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,52 @@
-public class Main
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+/**
+ * @author Mathias Gnadlinger
+ * @version 1.5, 15.10.2020
+ */
+public class Main extends Application
 {
     public static void main(String[] args)
     {
-        System.out.printf("Hello World");
+        Application.launch(args);
+    }
+    @Override
+    public void start (Stage stage)
+    {
+        VBox main_box = new VBox();
+        ObservableList data_list = main_box.getChildren();
+
+        EventHandler<MouseEvent> addHandler = new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+            }
+        };
+
+        EventHandler<MouseEvent> saveHandler = new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+            }
+
+        };
+
+        //Creating a Scene
+        Scene scene = new Scene(main_box);
+        //Setting tittle to the scene
+        stage.setTitle("Shopping List");
+        //Adding the scene to the group
+        stage.setScene(scene);
+        //Displaying the conntents of a scene
+        stage.show();
     }
 }
